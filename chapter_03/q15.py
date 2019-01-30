0ea53c1fd0bc8b7173b3e2feb5095ca81c1da8bf
Mehmet Mustafa Icer <mmustafaicer@gmail.com>
2019-01-29 20:44:34 -0600

    if structure changed
    The complete if structure has been constructed in a wrong way. It has been fixed.

--- chapter_03/q15.py
+++ chapter_03/q15.py
@@ -6,14 +6,14 @@ user_seconds=int(input("Enter the number of seconds: "))
 print()
 
 # create decision structure
-if user_seconds>=60:
-    minutes=user_seconds//60
-    print("There are ", minutes, "minutes in ", user_seconds, "seconds.")
+if user_seconds>=86400:
+    days=user_seconds//86400
+    print("There are", days, "days in", user_seconds, "seconds.")
 elif user_seconds>=3600:
     hours=user_seconds//3600
-    print("There are ", hours, "hours in", user_seconds, "seconds.")
-elif user_seconds>=86400:
-    days=user_seconds//86400
-    print("There are ", days, "days in", user_seconds, "seconds.")
+    print("There are", hours, "hours in", user_seconds, "seconds.")
+elif user_seconds>=60:
+    minutes=user_seconds//60
+    print("There are", minutes, "minutes in ", user_seconds, "seconds.")
 else:
     print("ERROR: Invalid Response.")
